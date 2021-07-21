ea523fa29e2824ef096ff7e8672443fdb503c230
migeuu <miguelmarkiori@gmail.com>
2021-07-21 17:53:47 -0300

    Mudanças na responsividade para novos dispositivos e JS para o botão email

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
+const footerEmail = document.getElementById("footer-email")
+const news = document.getElementById("footer-email-btn");
+
+    news.addEventListener("click", function() {
+        newsSubmit();
+    })
+
+    function newsSubmit(){
+        let text;
+        let emailX = footerEmail.value;
+        if(emailX == ""){
+            text = "Insira um e-mail válido."
+        } else {
+            text = "Email cadastrado com sucesso!"
+        }
+        document.getElementById("text-news").innerHTML = text;
+    }
+
 const slides = document.querySelector(".slider").children;
 const prev = document.querySelector(".prev")
 const next = document.querySelector(".next")
 const indicator = document.querySelector(".indicator")
 
-
-
-let index=0;
+let index = 0;
 
     prev.addEventListener("click", function(){
         prevSlide();
