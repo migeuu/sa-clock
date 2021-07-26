e230446cda6e53c715a88bdc794ec5513c239ad1
migeuu <miguelmarkiori@gmail.com>
2021-07-26 16:04:47 -0300

    Exclusão script Newsletter

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-const footerEmail = document.getElementById("footer-email")
-const news = document.getElementById("footer-email-btn");
-
-    news.addEventListener("click", function() {
-        newsSubmit();
-    })
-
-    function newsSubmit(){
-        let text;
-        let emailX = footerEmail.value;
-        if(emailX == ""){
-            text = "Insira um e-mail válido."
-        } else {
-            text = "Email cadastrado com sucesso!"
-            document.getElementById("text-news").innerHTML = text;
-        }
-    }
-
 const slides = document.querySelector(".slider").children;
 const prev = document.querySelector(".prev")
 const next = document.querySelector(".next")
